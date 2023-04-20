bc7ff870377e82a446736cf553ae10c1c224b541
Michael Zhou <mzhou@cse.unsw.edu.au>
2023-04-21 03:25:58 +0900

    try separate read thread

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,95 @@
+use std::sync::mpsc::channel;
+use std::thread::spawn;
+
 use anyhow::{Context, Result};
 use clap::Parser;
-use pcap::{Active, Capture, Device};
+use pcap::Capture;
 use pnet::datalink::MacAddr;
-use pnet::packet::ethernet::{EtherType, Ethernet, MutableEthernetPacket};
-use pnet::packet::Packet;
+use pnet::packet::ethernet::{EtherType, Ethernet, EthernetPacket, MutableEthernetPacket};
+use pnet::packet::{FromPacket, Packet};
 
 #[derive(Debug, Parser)]
 struct Args {
     #[arg(long, required = true)]
-    destination: MacAddr,
+    authenticator: MacAddr,
     #[arg(long, required = true)]
     interface: String,
+    #[arg(long, default_value_t = false)]
+    start: bool,
     #[arg(long, required = true)]
-    source: MacAddr,
+    supplicant: MacAddr,
+}
+
+const ETHERTYPE_8021X: EtherType = EtherType(0x888e);
+
+fn build_packet(e: &Ethernet) -> Option<MutableEthernetPacket<'static>> {
+    let mut packet =
+        MutableEthernetPacket::owned(vec![0u8; MutableEthernetPacket::packet_size(&e)])?;
+    packet.populate(e);
+    Some(packet)
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
 
-    let device = Device::list()
-        .context("Couldn't list devices")?
-        .into_iter()
-        .find(|d| d.name == args.interface)
-        .context("Interface not found")?;
+    let mut capture_read = Capture::from_device(args.interface.as_str())
+        .context("Capture::from_device failed for read")?
+        .promisc(true)
+        .open()
+        .context("Capture::open failed for read")?;
 
-    let mut capture: Capture<Active> = device.open().context("Couldn't open device")?;
-
-    capture
+    capture_read
         .filter("ether proto 0x888e", true)
-        .context("Couldn't apply filter")?;
+        .context("Capture::filter failed for read")?;
 
-    let id = 0x00u8;
+    let (read_tx, read_rx) = channel();
+    let read_thread = spawn(move || -> Result<()> {
+        loop {
+            let packet = capture_read.next_packet().context("next_packet failed")?;
+            read_tx
+                .send(packet.data.to_owned())
+                .context("send failed")?;
+        }
+    });
+
+    let mut capture_write = Capture::from_device(args.interface.as_str())
+        .context("Capture::from_device failed for write")?
+        .open()
+        .context("Capture::open failed for write")?;
+
+    if args.start {
+        let start = Ethernet {
+            destination: MacAddr::new(0x01, 0x80, 0xc2, 0x00, 0x00, 0x03),
+            ethertype: ETHERTYPE_8021X,
+            payload: vec![
+                // 802.1x
+                0x01, // version
+                0x01, // type
+                0x00, 0x00, // length
+                // padding (42 bytes)
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // padding (8 bytes)
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // padding (8 bytes)
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // padding (8 bytes)
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // padding (8 bytes)
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // padding (8 bytes)
+                0x00, 0x00, // padding (2 bytes)
+            ],
+            source: args.supplicant,
+        };
+
+        println!("S {:?}", start);
+
+        let start_packet = build_packet(&start).context("build_packet failed for start")?;
+        capture_write
+            .sendpacket(start_packet.packet())
+            .context("sendpacket failed for start")?;
+    }
+
+    let id = 0x1bu8;
 
     let request_identity = Ethernet {
-        destination: args.destination,
-        source: args.source,
-        ethertype: EtherType::new(0x888e),
+        destination: args.supplicant,
+        ethertype: ETHERTYPE_8021X,
         payload: vec![
             // 802.1x
             0x01, // version
@@ -46,19 +100,33 @@ fn main() -> Result<()> {
             id,   // id
             0x00, 0x05, // length
             0x01, // type
+            // padding (37 bytes)
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // padding (8 bytes)
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // padding (8 bytes)
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // padding (8 bytes)
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // padding (8 bytes)
+            0x00, 0x00, 0x00, 0x00, 0x00, // padding (5 bytes)
         ],
+        source: args.authenticator,
     };
 
-    let mut request_identity_packet = MutableEthernetPacket::owned(vec![
-            0u8;
-            MutableEthernetPacket::packet_size(&request_identity)
-        ])
-    .context("Couldn't allocate MutableEthernetPacket")?;
-    request_identity_packet.populate(&request_identity);
+    println!("S {:?}", request_identity);
 
-    capture
+    let request_identity_packet =
+        build_packet(&request_identity).context("build_packet failed for request_identity")?;
+
+    capture_write
         .sendpacket(request_identity_packet.packet())
-        .context("sendpacket failed")?;
+        .context("sendpacket for request_identity failed")?;
+
+    let response_identity_packet = read_rx
+        .recv()
+        .context("recv for response_identity failed")?;
+    let response_identity = &EthernetPacket::new(&response_identity_packet)
+        .context("EthernetPacket::new failed for response_identity")?
+        .from_packet();
+
+    println!("R {:?}", response_identity);
 
     Ok(())
 }
